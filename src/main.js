accf600f9b6ed9aaab12a45c1a7d111bbb2de464
werner <werner_a_e@yahoo.es>
2020-01-17 20:44:11 -0300

    Add some code to ignore a not needed message.
    related: https://github.com/vuetifyjs/vuetify/issues/9999

--- src/main.js
+++ src/main.js
@@ -7,6 +7,17 @@ Vue.use(VueTextareaAutosize)
 
 Vue.config.productionTip = false
 
+// To avoid some annoying warning
+// https://github.com/vuetifyjs/vuetify/issues/9999
+const ignoreWarnMessage = 'The .native modifier for v-on is only valid on components but it was used on <div>.'
+Vue.config.warnHandler = function (msg, vm, trace) {
+  trace
+  if (msg === ignoreWarnMessage) {
+    msg = null
+    vm = null
+    trace = null
+  }
+}
 new Vue({
   vuetify,
   render: h => h(App)
